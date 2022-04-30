77855819a5e7a421c97b1d1ee9b4d9d774a85133
unknown <hlvudat1206@gmail.com>
2022-04-30 14:11:01 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 // import ThreeScene from './threejs/three-scene2'
-=======
-import ThreeScene from './threejs/three-scene';
->>>>>>> 979174e0733bb193497d0a843b77fa25e44fb804
 import React, { Component }  from 'react';
 import Directurl from './router/directurl';
 
