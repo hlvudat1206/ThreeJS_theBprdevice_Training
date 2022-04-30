639a94919c3537f6b838f5bb2f017c86ee0c9298
unknown <hlvudat1206@gmail.com>
2022-04-30 14:09:16 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import ThreeScene from './threejs/three-scene'
+// import ThreeScene from './threejs/three-scene2'
+import ThreeScene from './threejs/three-scene';
+
 import React, { Component }  from 'react';
 import Directurl from './router/directurl';
 
