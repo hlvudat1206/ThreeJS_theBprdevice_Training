564500beac186e165481711db1132f0e6062c20e
unknown <hlvudat1206@gmail.com>
2022-05-18 23:28:36 +0700

    upload

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5001;
 const apiPath = '/api/';
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
