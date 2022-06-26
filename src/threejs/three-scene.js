bce3ad4208076dc0f2363a3c3efaae258af1955d
VuDat <hlvudat1206@gmail.com>
2022-06-26 21:49:27 +0700

    ClientSide

--- src/threejs/three-scene.js
+++ src/threejs/three-scene.js
@@ -77,7 +77,6 @@ export default class ThreeScene extends Component {
       
 
       camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      // camera = new THREE.PerspectiveCamera( 185, window.innerWidth / window.innerHeight, 0.1, 1000 );
       camera.position.set(15, 2, 0);//wide position
       // camera.position.set(10, 0, 0);
       camera.lookAt(0,1.5,0);
@@ -1353,4 +1352,3 @@ export default class ThreeScene extends Component {
   
 }
   
-    
